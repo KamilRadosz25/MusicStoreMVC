d990f2398e1be36b5b6a2d3bf0222d89375000f4
unknown <kamil.radosz.25@gmail.com>
2022-10-21 15:59:15 +0200

    Initialize new project

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+        public string Browse()
+        {
+            return "Hello From Store.Browse()";
+        }
+        public string Details()
+        {
+            return "Hello from Store.Details()";
+        }
+
+    }
+}
