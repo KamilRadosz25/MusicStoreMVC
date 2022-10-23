51f3202f51a7306e42281db043b5280daadccb6d
Kamil Radosz <kamil.radosz.25@gmail.com>
2022-10-23 22:14:03 +0200

    Add Validation for Album Model

--- MvcMusicStore/Models/Album.cs
+++ MvcMusicStore/Models/Album.cs
@@ -1,14 +1,29 @@
-﻿namespace MvcMusicStore.Models
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcMusicStore.Models
 {
+
     public class Album
     {
+        [ScaffoldColumn(false)]
+        [BindNever]
         public int AlbumId { get; set; }
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
         public string Title { get; set; }
+        [Range(0.01, 100.00, ErrorMessage = "Price must be between 0.01 and 100.00")]
         public decimal Price { get; set; }
+        [DisplayName("Album Art URL")]
+        [StringLength(1024)]
         public string AlbumArtUrl { get; set; }
-        public Genre? Genre { get; set; }
-        public Artist? Artist { get; set; }
+        public virtual Genre? Genre { get; set; }
+        public virtual Artist? Artist { get; set; }
     }
 }
